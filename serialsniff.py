2c1187a17e83871cc4afd1f59fc72150d26fa8db
Renz Christian Bagaporo <renz.bagaporo@gmail.com>
2020-04-05 10:40:52 +0800

    Enforce default flake8 formatting rules

--- serialsniff.py
+++ serialsniff.py
@@ -1,14 +1,16 @@
+# Copyright (c) 2020 Renz Christian Bagaporo
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
 import serial
 import os
-import time
 import enum
 import threading
 import select
 import queue
-import collections
 import argparse
 
-from abc import ABC, abstractmethod
 from datetime import datetime
 
 
@@ -33,7 +35,8 @@ class SerialSniff(threading.Thread):
             self.direction = direction
 
     def _output_data_to_queue(self, data, direction):
-        if self.mode == SerialSniff.Mode.BIDIRECTIONAL or self.mode == direction:
+        if self.mode == SerialSniff.Mode.BIDIRECTIONAL or \
+           self.mode == direction:
             self.data_queue.put(SerialSniff.Data(data, direction))
 
     def _incoming_data_thread_fn(self):
@@ -45,17 +48,20 @@ class SerialSniff(threading.Thread):
             events = poll.poll()
             for fd, event_type in events:
                 data = os.read(self._pty_master, self._max_data_len)
-                self._output_data_to_queue(data, SerialSniff.Direction.INCOMING)
+                self._output_data_to_queue(data,
+                                           SerialSniff.Direction.INCOMING)
                 self._serial.write(data)
 
     def _outgoing_data_thread_fn(self):
         while True:
             data = self._serial.read(self._max_data_len)
             if data:
-                self._output_data_to_queue(data, SerialSniff.Direction.OUTGOING)
+                self._output_data_to_queue(data,
+                                           SerialSniff.Direction.OUTGOING)
                 os.write(self._pty_master, data)
 
-    def __init__(self, port, baud, mode, timeout=DEFAULT_TIMEOUT, max_data_len=DEFAULT_MAX_DATA_LEN):
+    def __init__(self, port, baud, mode, timeout=DEFAULT_TIMEOUT,
+                 max_data_len=DEFAULT_MAX_DATA_LEN):
         self._pty_master, self._pty_slave = os.openpty()
 
         self.port = port
@@ -69,9 +75,19 @@ class SerialSniff(threading.Thread):
 
         self.mode = mode
 
-        self._serial = serial.Serial(self.port, self.baud, rtscts=True, dsrdtr=True, timeout=self.timeout_s)
-        self._incoming_data_thread = threading.Thread(target=self._incoming_data_thread_fn, daemon=True)
-        self._outgoing_data_thread = threading.Thread(target=self._outgoing_data_thread_fn, daemon=True)
+        self._serial = serial.Serial(self.port,
+                                     self.baud,
+                                     rtscts=True,
+                                     dsrdtr=True,
+                                     timeout=self.timeout_s)
+        self._incoming_data_thread = threading.Thread(
+                                    target=self._incoming_data_thread_fn,
+                                    daemon=True
+                                    )
+        self._outgoing_data_thread = threading.Thread(
+                                    target=self._outgoing_data_thread_fn,
+                                    daemon=True
+                                    )
 
         self.data_queue = queue.Queue()
 
@@ -88,11 +104,14 @@ class SerialSniff(threading.Thread):
         os.close(self._pty_slave)
         os.close(self._pty_master)
 
+
 def sniffer_log_thread_fn(data_queue, columns):
     while True:
         data = data_queue.get()
 
-        direction = "[INCOMING]" if data.direction == SerialSniff.Direction.INCOMING else "[OUTGOING]"
+        direction = "[INCOMING]" \
+                    if data.direction == SerialSniff.Direction.INCOMING \
+                    else "[OUTGOING]"
         timestamp = data.time.strftime("%H:%M:%S.%f")
         data_len = len(data.data)
 
@@ -106,9 +125,12 @@ def sniffer_log_thread_fn(data_queue, columns):
             data_str = str(data_bytes)[2:-1]
 
             data_hex_str = data_bytes.hex()
-            data_hex_str = ' '.join(a+b for a,b in zip(data_hex_str[::2], data_hex_str[1::2]))
+            data_hex_str = ' '.join(a+b for a, b in zip(data_hex_str[::2],
+                                                        data_hex_str[1::2]))
 
-            print("\t{} {}".format(data_str.ljust(columns * 4, ' '), data_hex_str)) # 4 is worst case, ex: ctrl-z = \x1a
+            # 4 is worst case, ex: ctrl-z = \x1a
+            print("\t{} {}".format(data_str.ljust(columns * 4, ' '),
+                                   data_hex_str))
 
             start = start + columns
             data_bytes = data.data[start:start+columns]
@@ -116,6 +138,7 @@ def sniffer_log_thread_fn(data_queue, columns):
             print(" " * len(details), end='')
 
         print("\r\n")
+
 
 def main():
     try:
@@ -129,11 +152,17 @@ def main():
         args = parser.parse_args()
 
         if args.incoming and args.outgoing:
-            sniff = SerialSniff(args.port, args.baudrate, SerialSniff.Mode.BIDIRECTIONAL)
+            sniff = SerialSniff(args.port,
+                                args.baudrate,
+                                SerialSniff.Mode.BIDIRECTIONAL)
         elif args.incoming:
-            sniff = SerialSniff(args.port, args.baudrate, SerialSniff.Mode.INCOMING_ONLY)
-        else: # outgoing, or default - sniff FROM_SERIAL
-            sniff = SerialSniff(args.port, args.baudrate, SerialSniff.Mode.OUTGOING_ONLY)
+            sniff = SerialSniff(args.port,
+                                args.baudrate,
+                                SerialSniff.Mode.INCOMING_ONLY)
+        else:  # outgoing, or default - sniff FROM_SERIAL
+            sniff = SerialSniff(args.port,
+                                args.baudrate,
+                                SerialSniff.Mode.OUTGOING_ONLY)
 
         print("SerialSniff\r\n")
         print("port:\t\t" + sniff.port)
@@ -142,7 +171,10 @@ def main():
 
         print("\r\n")
 
-        sniffer_log_thread = threading.Thread(target=sniffer_log_thread_fn, args=(sniff.data_queue, args.columns), daemon=True)
+        sniffer_log_thread = threading.Thread(target=sniffer_log_thread_fn,
+                                              args=(sniff.data_queue,
+                                                    args.columns),
+                                              daemon=True)
 
         sniffer_log_thread.start()
         sniff.start()
@@ -152,6 +184,6 @@ def main():
     except KeyboardInterrupt:
         sniff.cleanup()
 
+
 if __name__ == "__main__":
     main()
-
