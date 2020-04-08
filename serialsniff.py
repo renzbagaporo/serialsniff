85369941f64f65cf0d32abff293cf4c935ae32b6
Renz Christian Bagaporo <renz.bagaporo@gmail.com>
2020-04-08 21:53:31 +0800

    Remove more flake8 warnings

--- serialsniff.py
+++ serialsniff.py
@@ -106,13 +106,13 @@ class SerialSniff(threading.Thread):
 
 
 def output(s, e=None, f=None):
-    if not e == None:
+    if e is not None:
         print(s, end=e)
     else:
         print(s)
 
     if f:
-        f.write(s + ("\r\n" if e == None else e))
+        f.write(s + ("\r\n" if e is None else e))
         f.flush()
 
 
@@ -141,7 +141,7 @@ def sniffer_log_thread_fn(data_queue, columns, out_file=None):
 
             # 4 is worst case, ex: ctrl-z = \x1a
             output("\t{} {}".format(data_str.ljust(columns * 4, ' '),
-                                   data_hex_str), f=out_file)
+                                    data_hex_str), f=out_file)
 
             start = start + columns
             data_bytes = data.data[start:start+columns]
@@ -154,12 +154,21 @@ def sniffer_log_thread_fn(data_queue, columns, out_file=None):
 def main():
     try:
         parser = argparse.ArgumentParser()
-        parser.add_argument("port", type=str, help="serial port to sniff")
-        parser.add_argument("baudrate", type=int, help="baudrate of the serial port to sniff")
-        parser.add_argument("--incoming", "-i", action="store_true", help="sniff incoming (from host to serial device) transfers")
-        parser.add_argument("--outgoing", "-o", action="store_true", help="sniff outgoing (from serial device to host) transfers")
-        parser.add_argument("--file", "-f", type=argparse.FileType("w"), help="file to write the sniffed transfers to")
-        parser.add_argument("--columns", type=int, default=8, help="how many columns of characters to display per line")
+        parser.add_argument("port", type=str,
+                            help="serial port to sniff")
+        parser.add_argument("baudrate", type=int,
+                            help="baudrate of the serial port to sniff")
+        parser.add_argument("--incoming", "-i", action="store_true",
+                            help="sniff incoming (from host to serial device) \
+                            transfers")
+        parser.add_argument("--outgoing", "-o", action="store_true",
+                            help="sniff outgoing (from serial device to host) \
+                            transfers")
+        parser.add_argument("--file", "-f", type=argparse.FileType("w"),
+                            help="file to write the sniffed transfers to")
+        parser.add_argument("--columns", type=int, default=8,
+                            help="how many columns of characters to display \
+                            per line")
 
         args = parser.parse_args()
 
